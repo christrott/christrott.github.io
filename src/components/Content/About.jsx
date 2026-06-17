867822c408bb15d5c54d4e8449a198dc8aa1be1d
Chris Trott <christrott86@gmail.com>
2026-06-17 11:35:18 +1000

    update about me text

--- src/components/Content/About.jsx
+++ src/components/Content/About.jsx
@@ -13,13 +13,13 @@ export default class About extends React.Component {
                         <div className="content">
                             <h3 className="title is-3">About Me</h3>
                             <p>
-                                Hi, I'm  Chris Trott. I'm a web developer with experience as a Front End and Full Stack Developer, specifically working with Web Apps.
-                                I mostly work with JavaScript and C#, and my most recent experience has been a mix of React and Node.js.
+                                Hi, I'm  Chris Trott. I'm a software engineer with experience as a Front End and Full Stack Developer, specifically working with Web Apps.
+                                I am most comfortable working with JavaScript and Node.js, with most of my recent experience working with React and PHP.
                                 I have targetted experience with Product-based companies that solve problems within a specific domain.
                                 I build software with precision and integrity, and pride myself on my attention to detail.
-                                In my spare time, I try out new technology and build small games in Unity, Unreal Engine, or HTML Canvas.
-                                I live with my wife and 4 kids in Melbourne, Victoria.
-                                When I'm not experimenting and creating with tech, I'm out enjoying time with the family and soaking up the awesome Melbourne culture.
+                                In my spare time, I try out new technology and build small games in Unity, Godot, or HTML Canvas.
+                                When I'm not experimenting and creating with tech, I'm out enjoying time with the family, 
+                                getting involved in my local Soccer club by coaching, playing and other volunteer work.
                             </p>
                         </div>
                     </div>
